084fa98b013cd67ee0e07ed83a58e50afc347987
wei yuan <737571078@qq.com>
2021-02-24 15:52:14 +0800

    增加can连接已经cav存储类

--- EngineRev/Connection/CANConnection.cs
+++ EngineRev/Connection/CANConnection.cs
@@ -134,40 +134,65 @@ namespace EngineRev
 		/// <returns></returns>
 		/// 导入can驱动
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_OpenDevice(UInt32 DeviceType, UInt32 DeviceInd, UInt32 Reserved);
+		/// <summary>
+		/// 打开设备
+		/// </summary>
+		public static extern UInt32 VCI_OpenDevice(UInt32 DeviceType, UInt32 DeviceInd, UInt32 Reserved);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_CloseDevice(UInt32 DeviceType, UInt32 DeviceInd);
+		/// <summary>
+		/// 关闭设备
+		/// </summary>
+		public static extern UInt32 VCI_CloseDevice(UInt32 DeviceType, UInt32 DeviceInd);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_InitCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_INIT_CONFIG pInitConfig);
+		/// <summary>
+		/// 设备初始化
+		/// </summary>
+		public static extern UInt32 VCI_InitCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_INIT_CONFIG pInitConfig);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_ReadBoardInfo(UInt32 DeviceType, UInt32 DeviceInd, ref VCI_BOARD_INFO pInfo);
+
+		public static extern UInt32 VCI_ReadBoardInfo(UInt32 DeviceType, UInt32 DeviceInd, ref VCI_BOARD_INFO pInfo);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_ReadErrInfo(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_ERR_INFO pErrInfo);
+		public static extern UInt32 VCI_ReadErrInfo(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_ERR_INFO pErrInfo);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_ReadCANStatus(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_CAN_STATUS pCANStatus);
+		public static extern UInt32 VCI_ReadCANStatus(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_CAN_STATUS pCANStatus);
 
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_GetReference(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, UInt32 RefType, ref byte pData);
+		public static extern UInt32 VCI_GetReference(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, UInt32 RefType, ref byte pData);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_SetReference(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, UInt32 RefType, ref byte pData);
+		public static extern UInt32 VCI_SetReference(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, UInt32 RefType, ref byte pData);
 
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_GetReceiveNum(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+		/// <summary>
+		/// 获取接受区字节数
+		/// </summary>
+		public static extern UInt32 VCI_GetReceiveNum(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_ClearBuffer(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+		public static extern UInt32 VCI_ClearBuffer(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
 
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_StartCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+		/// <summary>
+		/// 打开can连接
+		/// </summary>
+		public static extern UInt32 VCI_StartCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_ResetCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
+		/// <summary>
+		/// 关闭can连接
+		/// </summary>
+		public static extern UInt32 VCI_ResetCAN(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd);
 
 		[DllImport("controlcan.dll")]
-		static extern UInt32 VCI_Transmit(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_CAN_OBJ pSend, UInt32 Len);
+		/// <summary>
+		/// 发送can信息
+		/// </summary>
+		public static extern UInt32 VCI_Transmit(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_CAN_OBJ pSend, UInt32 Len);
 
 		//[DllImport("controlcan.dll")]
 		//static extern UInt32 VCI_Receive(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, ref VCI_CAN_OBJ pReceive, UInt32 Len, Int32 WaitTime);
 		[DllImport("controlcan.dll", CharSet = CharSet.Ansi)]
-		static extern UInt32 VCI_Receive(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, IntPtr pReceive, UInt32 Len, Int32 WaitTime);
+		/// <summary>
+		/// 接受数据
+		/// </summary>
+		public static extern UInt32 VCI_Receive(UInt32 DeviceType, UInt32 DeviceInd, UInt32 CANInd, IntPtr pReceive, UInt32 Len, Int32 WaitTime);
 
 
 	}
